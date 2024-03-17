e2e87704cc1adf8277533eb331620f551b92d281
Md Adil Hussain <adil198@Mds-MacBook-Air-2.local>
2024-03-17 12:57:03 +0530

    Pass stage-5

--- app/server.go
+++ app/server.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -32,13 +33,70 @@ func main() {
 func handleConnection(c net.Conn) {
 	defer c.Close()
 	for {
-		input := make([]byte, 20)
-		n, err := c.Read(input)
+		input := make([]byte, 1024)
+		input_size, err := c.Read(input)
 		if err != nil {
 			fmt.Printf("Error %v. Exiting\n", err.Error())
 			return
 		}
-		fmt.Printf("Recievied %v bytes from client\n", n)
-		c.Write([]byte("+PONG\r\n"))
+		fmt.Printf("Recievied %v bytes from client\n", input_size)
+		fmt.Printf("Length of input from client: %v\n", len(string(input)))
+		cl_cmd := parseInput(string(input))
+		execute(cl_cmd, c)
 	}
+}
+
+type clientCommand struct {
+	command string
+	args    []string
+}
+
+func parseInput(command_str string) clientCommand {
+	var cl_cmd clientCommand
+	parser := Parser(command_str)
+	data, err := parser.getNext()
+	if err != nil || data.dataType != RESP_ARRAY {
+		cl_cmd.command = "INVALID"
+		cl_cmd.args = make([]string, 0)
+		return cl_cmd
+	}
+	args_count, err := strconv.Atoi(data.value)
+	args_count--
+	if err != nil {
+		cl_cmd.command = "INVALID"
+		cl_cmd.args = make([]string, 0)
+		return cl_cmd
+	}
+	fmt.Println("Args count: ", args_count)
+
+	for ; args_count+1 > 0 && parser.hasNext(); args_count-- {
+		data, err := parser.getNext()
+		if err != nil || data.dataType != RESP_BULK {
+			cl_cmd.command = "INVALID"
+			cl_cmd.args = make([]string, 0)
+			return cl_cmd
+		}
+		if len(cl_cmd.command) == 0 {
+			cl_cmd.command = strings.ToUpper(data.value)
+		} else {
+			cl_cmd.args = append(cl_cmd.args, data.value)
+			fmt.Println(cl_cmd.args)
+		}
+	}
+	return cl_cmd
+}
+
+func execute(cl_cmd clientCommand, c net.Conn) {
+	switch cl_cmd.command {
+	case ECHO:
+		c.Write(make_Output(cl_cmd.args[0]))
+	case PING:
+		c.Write(make_Output("PONG"))
+	default:
+		c.Write(make_Output("Invalid input"))
+	}
+}
+
+func make_Output(str string) []byte {
+	return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(str), str))
 }
---
+++ app/utils.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+const (
+	RESP_ARRAY = '*'
+	RESP_SIMPLE = '+'
+	RESP_BULK = '$'
+	RESP_TERM = "\r\n"
+	ECHO = "ECHO"
+	PING = "PING"
+)
+
+type RESP_Parser struct {
+	str string
+	position int
+}
+
+type RESP_Data struct {
+	dataType rune
+	value string
+}
+
+func (p *RESP_Parser) hasNext() (bool) {
+	return p.position < len(p.str)
+}
+
+func (p *RESP_Parser) getNext() (RESP_Data, error) {
+	var data RESP_Data
+
+	if p.position >= len(p.str) {
+		fmt.Println("Error: Position of parser exceeded length of input string")
+		return invalidInput()
+	}
+
+	c := p.str[p.position]
+	p.position++
+	switch c {
+		case RESP_ARRAY:
+			data.dataType = RESP_ARRAY
+			end := p.position
+			for end+2 < len(p.str) && p.str[end:end+2] != RESP_TERM {
+				end++
+			}
+			data.value = p.str[p.position:end]
+			p.position = end+2
+		case RESP_BULK:
+			data.dataType = RESP_BULK
+			end := p.position
+			for end+2 < len(p.str) && p.str[end:end+2] != RESP_TERM {
+				end++
+			}
+			length, err := strconv.Atoi(p.str[p.position:end])
+			if err != nil {return invalidInput()}
+			p.position = end+2
+
+			end = p.position
+			for end+2 < len(p.str) && p.str[end:end+2] != RESP_TERM {
+				end++
+			}
+			if length != end-p.position {return invalidInput()}
+			data.value = p.str[p.position:end]
+			p.position = end+2
+		default:
+			return invalidInput()
+	}
+	return data, nil
+}
+
+func invalidInput() (RESP_Data, error) {
+	err := errors.New("INVALID INPUT")
+	return RESP_Data{}, err
+}
+
+func Parser(s string) (RESP_Parser) {
+	return RESP_Parser{str: s, position: 0}
+}
